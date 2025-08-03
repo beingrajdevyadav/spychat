fa73a2628b5e1c4db4f238027d6c40c946263045
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-08-03 23:09:04 +0530

    show functions added.

--- public/client.js
+++ public/client.js
@@ -13,8 +13,29 @@ const sendBtn = document.getElementById("send-btn");
 const chatsBody = document.getElementById("chats-body");
 
 // function to get IP Address
-function getIP(){
+function getIP() {
     return fetch("https://api.ipify.org?format=json")
-    .then(res=>res.json)
-    .then(data=>data.ip);
+        .then(res => res.json)
+        .then(data => data.ip);
+};
+
+// show register
+function showHero() {
+    hero.style.display = 'block';
+    register.style.display = "none";
+    chats.style.display = "none";
+};
+
+// show register
+function showRegister(){
+    hero.style.display = "none";
+    register.style.display = "flex";
+    chats.style.display = "none";
+}
+
+// show chat
+function showChat(){
+    hero.style.display = "none";
+    register.style.display = "none";
+    chats.style.display = "block";
 }
