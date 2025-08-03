13db7294d2a86a23c90866600d2e76cbb9e548b3
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-08-03 21:53:19 +0530

    server created successfully.

---
+++ src/server.js
@@ -0,0 +1,18 @@
+const express = require("express") ;
+const http = require("http") ;
+const { Server } = require("socket.io") ;
+const path = require("path") ;
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static(path.join(__dirname, "../public")));
+
+
+const PORT = 3000;
+
+// server listening
+server.listen(PORT, ()=>{
+    console.log(`server is serving at http://localhost${PORT}`);
+})
